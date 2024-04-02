73ff41f71fbce37b0896fcca765233272704b5ca
AndreyUrazaev <f.a.z.a@bk.ru>
2024-04-02 12:39:22 +0500

    lesson-22-useEffect-setTimeout-setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -35,3 +35,47 @@ export const SimpleExample = () => {
     </div>
   </>
 }
+
+export const SetTimeoutExample = () => {
+  const [fake, setFake] = useState(1);
+  const [counter, setCounter] = useState(1);
+  console.log("SetTimeoutExample - " + counter)
+  
+  useEffect(() => {
+    setInterval(() => {
+      setCounter((s) => s + 1)
+    }, 1000)
+  }, []);
+  
+  
+  return <>
+    <div>
+      <button onClick={() => setFake(fake + 1)}>fake</button>
+      {fake}
+    </div>
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>counter</button>
+      {counter}
+    </div>
+  </>
+}
+
+export const ClockExample = () => {
+  
+  const hours = new Date().getHours()
+  const minutes = new Date().getMinutes()
+  const seconds = new Date().getSeconds()
+  
+  const [second, setSecond] = useState(seconds);
+  
+  useEffect(() => {
+    setInterval(() => {
+      setSecond(s => s < 59 ? s + 1 : 0)
+    }, 1000)
+  }, []);
+  
+  
+  return <>
+    <span>{hours} : {minutes} : {second}</span>
+  </>
+}
