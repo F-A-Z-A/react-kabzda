a0658d7621739f3fbec57a539a35b969d0b07964
AndreyUrazaev <f.a.z.a@bk.ru>
2024-10-15 10:09:59 +0500

    lesson-13-controlled-input-checkbox-select

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -44,5 +44,52 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 };
 
 export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState("");
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.value);
+  };
+
+  return (
+    <>
+      <input value={parentValue} onChange={onChange} />
+    </>
+  );
+};
+
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState(false);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.checked);
+  };
+
+  return (
+    <>
+      <input type={"checkbox"} checked={parentValue} onChange={onChange} />
+    </>
+  );
+};
+
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>(undefined);
+
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setParentValue(e.currentTarget.value);
+  };
+
+  return (
+    <>
+      <select value={parentValue} onChange={onChange}>
+        <option>none</option>
+        <option value="1">Minsk</option>
+        <option value="2">Moscow</option>
+        <option value="3">Nadym</option>
+      </select>
+    </>
+  );
+};
+
+export const ControlledInputWithFixedValue = () => {
   return <input value={"Hard Value"} />;
 };
