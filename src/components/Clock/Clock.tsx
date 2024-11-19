2ebd1d022a815d167f92e17576d8088f4c371569
AndreyUrazaev <f.a.z.a@bk.ru>
2024-11-19 10:08:32 +0500

    lesson-23-useEffect component clock

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+const get2digitsString = (num: number) => (num < 10 ? "0" + num : "" + num);
+
+type Props = {};
+export const Clock = (props: Props) => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log("TICK");
+      setDate(new Date());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      <span>{get2digitsString(date.getHours())}</span>:<span>{get2digitsString(date.getMinutes())}</span>:
+      <span>{get2digitsString(date.getSeconds())}</span>
+    </div>
+  );
+};
